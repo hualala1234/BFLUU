be5f3fc389bb6c69aa599342ed0ebaaa1c4cf224
karaage0o <gina98239@gmail.com>
2024-11-16 13:22:36 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -148,17 +148,6 @@ document.addEventListener("DOMContentLoaded", function () {
     
     // 初始檢查表單狀態
     checkFormValidity();
-    
-    // 清空表單欄位的函式
-    form.addEventListener("submit", function (event) {
-        // 確認表單的action為Google Form並使用target='_blank'
-        event.preventDefault();
-        form.submit(); // 在新視窗提交表單
-        // 清除欄位內容
-        nameField.value = "";
-        messageField.value = "";
-        checkFormValidity(); // 更新按鈕狀態
-    });
 });
 
 
