8fd7e6f6d009c29b63d2267631c53cbf4440e69e
karaage0o <gina98239@gmail.com>
2024-11-21 13:12:32 +0800

    t

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -243,3 +234,12 @@ document.addEventListener("DOMContentLoaded", function () {
       }
   });
 });
+
+
+document.addEventListener('contextmenu', (event) => event.preventDefault());
+            document.addEventListener('keydown', (event) => {
+                // 禁止 F12、Ctrl+Shift+I
+                if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I")) {
+                    event.preventDefault();
+                }
+            });
